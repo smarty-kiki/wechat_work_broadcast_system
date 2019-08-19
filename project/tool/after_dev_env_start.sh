ce2f637857293a9292a3d716768e47e12d115e6f
yaoyang <kiki@smarty.so>
2019-08-19 14:38:39 +0800

    Fork from git@github.com:smarty-kiki/mvc_frame.git and modify project name

--- project/tool/after_dev_env_start.sh
+++ project/tool/after_dev_env_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-php /var/www/mvc_frame/public/cli.php migrate:install
-php /var/www/mvc_frame/public/cli.php migrate
+php /var/www/layui_mvc_frame/public/cli.php migrate:install
+php /var/www/layui_mvc_frame/public/cli.php migrate
--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -4,11 +4,11 @@ ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../..
 
 sh $ROOT_DIR/project/tool/dep_build.sh link
 
-sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name mvc_frame \
+sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name layui_mvc_frame \
     -v $ROOT_DIR/../frame:/var/www/frame \
-    -v $ROOT_DIR/:/var/www/mvc_frame \
-    -v $ROOT_DIR/project/config/development/nginx/mvc_frame.conf:/etc/nginx/sites-enabled/default \
+    -v $ROOT_DIR/:/var/www/layui_mvc_frame \
+    -v $ROOT_DIR/project/config/development/nginx/layui_mvc_frame.conf:/etc/nginx/sites-enabled/default \
     -v $ROOT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
     -e 'TIMEZONE=Asia/Shanghai' \
-    -e 'AFTER_START_SHELL=/var/www/mvc_frame/project/tool/after_dev_env_start.sh' \
+    -e 'AFTER_START_SHELL=/var/www/layui_mvc_frame/project/tool/after_dev_env_start.sh' \
 kikiyao/debian_php_dev_env start
