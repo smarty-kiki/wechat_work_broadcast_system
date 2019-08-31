9d12fab619922ef05570b5b55a07b9df4be7cd8a
yaoyang <kiki@smarty.so>
2019-08-31 14:29:11 +0800

    添加 controller 根据模版生成的能力

--- project/tool/after_dev_env_start.sh
+++ project/tool/after_dev_env_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-php /var/www/layui_mvc_frame/public/cli.php migrate:install
-php /var/www/layui_mvc_frame/public/cli.php migrate
+ENV=development php /var/www/layui_mvc_frame/public/cli.php migrate:install
+ENV=development php /var/www/layui_mvc_frame/public/cli.php migrate
