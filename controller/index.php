d518f22ea0eb2357b3ebd36fc1ff7dfc599e3711
yaoyang <kiki@smarty.so>
2019-10-10 18:01:18 +0800

    添加主框架

--- controller/index.php
+++ controller/index.php
@@ -3,6 +3,24 @@
 if_get('/', function ()
 {
     return render('index/index', [
-        'text' => 'hello world',
+        'tree_infos' => [
+            [
+                'name' => '模块',
+                'key'  => 'module',
+                'icon_class' => 'layui-icon-component',
+                'children' => [
+                    [
+                        'name' => '页面',
+                        'key'  => 'page',
+                        'href'  => '/pages',
+                    ],
+                ],
+            ]
+        ],
     ]);
+});
+
+if_get('/dashboard', function ()
+{
+    return 'dashboard';
 });
